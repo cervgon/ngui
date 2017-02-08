1871b85cc9b347d8f1803d0a956e1410e2c2c49b
Gonzalo Cervantes <cervgon@gmail.com>
2017-02-08 14:05:05 -0300

    Split long CSSs

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import angular from 'angular';
 
 import cssReset from './css/reset.css';
 import cssCore from './css/core.css';
+import cssCore from './css/grid.css';
 import cssButtons from './css/button.element.css';
+import cssButtons from './css/label.element.css';
 
 const main = angular.module('ngui', []);
 export default main;
