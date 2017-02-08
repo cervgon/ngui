cfe53d3eca112709dd2f858b500fc9978620e190
Gonzalo Cervantes <cervgon@gmail.com>
2017-02-08 14:30:08 -0300

    Fix declarations

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import angular from 'angular';
 
 import cssReset from './css/reset.css';
 import cssCore from './css/core.css';
-import cssCore from './css/grid.css';
+import cssGrid from './css/grid.css';
 import cssButtons from './css/button.element.css';
-import cssButtons from './css/label.element.css';
+import cssLabels from './css/label.element.css';
 
 const main = angular.module('ngui', []);
 export default main;
