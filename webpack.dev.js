d033931f8346f5caa04223a76e2d01bf2a57b544
Saissaken <juansaissac@gmail.com>
2017-02-15 17:45:29 -0300

    Fix typo

--- webpack.dev.js
+++ webpack.dev.js
@@ -35,7 +35,7 @@ module.exports = {
             }
         ]
     },
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
     devServer: {
         // inline: true,
         host: "0.0.0.0"
