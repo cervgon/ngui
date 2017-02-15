81bff9f318d21c3b3f87a6441792135dcbc30439
Saissaken <juansaissac@gmail.com>
2017-02-15 15:32:19 -0300

    Add script to build css only

---
+++ webpack.css.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+console.log("CSS ONLY BUILD");
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'deleteme.js',
+        // libraryTarget: "umd",
+        // library: "ngui",
+        // umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+            },
+            {
+                test: /\.html$/,
+                use: 'ignore-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [/node_modules/]
+            }
+        ]
+    },
+    // devtool: 'source-map',
+    plugins: [new ExtractTextPlugin("styles.css")]
+};
