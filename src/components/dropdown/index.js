36a799ac4c375d1100cf4ee18ac85b8f44790d8f
Saissaken <juansaissac@gmail.com>
2017-03-01 19:12:34 -0300

    Update dropdown to new nomenclature

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -5,35 +5,53 @@ import Styles from './styles.css';
 export default main.component('dropdown', {
     template: Template,
     bindings: {
-        nguiOptions: '=',
-        nguiPlaceholder: '@',
-        nguiBordered: '@',
-        disabled: '@'
+        nguiModel: '=',
+        nguiOptions: '<',
+        // nguiPlaceholder: '@',
+        // nguiBordered: '@',
+        // nguiDisabled: '@'
     },
     controller: function($timeout) {
         "ngInject";
         var $ctrl = this;
+
         $ctrl.onFocus = function() {
-            if($ctrl.disabled)
+            if ($ctrl.disabled)
                 return;
             $ctrl.opened = true;
         }
+
         $ctrl.onBlur = function() {
-            $timeout(function(){
+            $timeout(function() {
                 $ctrl.opened = false;
-            },200);
+            }, 200);
         }
-        $ctrl.changeOption = function(option){
-            $ctrl.selectedOption = option;
-            console.log($ctrl.selectedOption);
+
+        $ctrl.changeOption = function(option) {
+            $ctrl.nguiModel = option;
+
+            // $ctrl.selectedOption = option;
+            // $ctrl.nguiModel = option;
             $ctrl.opened = false;
+            console.log("Dropdown value changed to", $ctrl.nguiModel);
+            document.activeElement.blur();
         }
-        $ctrl.$onInit = function (){
-            $ctrl.opened = false;
-            $ctrl.selectedOption = $ctrl.nguiPlaceholder;
-            if($ctrl.nguiBordered !== undefined){
-                $ctrl.bordered = true;
-            }
+
+        $ctrl.$onInit = function() {
+            console.log("this.nguiOptions in $onInit", this.nguiOptions);
+            var initialValue = this.nguiOptions;
+            $ctrl.options = initialValue.options || [];
+            $ctrl.opened = initialValue.opened || false;
+            $ctrl.placeholder = initialValue.placeholder || "Select";
+            $ctrl.bordered = initialValue.bordered || false;
+            $ctrl.disabled = initialValue.disabled || false;
+            // $ctrl.selectedOption = initialValue.placeholder || null;
+            // $ctrl.options = JSON.parse($ctrl.nguiOptions);
+            // $ctrl.opened = false;
+            // $ctrl.selectedOption = $ctrl.nguiPlaceholder;
+            // if($ctrl.nguiBordered !== undefined){
+            //     $ctrl.bordered = true;
+            // }
         }
     }
 });
