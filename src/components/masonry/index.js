088c3d7246ae4dfd790f1aac7a1bf27cf7b79da3
Gonzalo Cervantes <cervgon@gmail.com>
2018-08-16 17:29:19 -0300

    Masonry first working version

--- src/components/masonry/index.js
+++ src/components/masonry/index.js
@@ -21,22 +21,46 @@ export default angular
                 if (options){
                     $ctrl.bricks = options.bricks || '';
                     $ctrl.alignment = options.alignment || '';
+                    $ctrl.colWidth = options.width || 0;
                 }
 
-                var masornyDiv;
-                var innerWidth;
+                var masonryDiv;
+                var innerWidth = 0;
 
                 $timeout(function(){
-                    function masorny(){
-                        console.log($element);
-                        masornyDiv = $element[0].children[0];
-                        innerWidth = masornyDiv.children[0].clientWidth;
 
-                        // Masorny Elements
-                        var masornyElements = masornyDiv.children[0].children;
-                        var masornyElementsTransclude = masornyDiv.children[0].lastElementChild.children;
+                    //console.log($element);
 
-                        if(!masornyElements && !masornyElementsTransclude) return;
+                    masonryDiv = $element[0].children[0];
+
+                    if(!$ctrl.alignment && $ctrl.colWidth > 0){
+                        masonryDiv.children[0].style.margin = 0;
+                    }
+
+                    function masonry(){
+
+                        if($ctrl.colWidth > 0){
+                            var counter = 1;
+                            innerWidth = 0;
+                            while($ctrl.colWidth * counter < masonryDiv.clientWidth){
+                                innerWidth += $ctrl.colWidth;
+                                counter++;
+                            }
+                        } else{
+                            innerWidth = masonryDiv.children[0].clientWidth;
+                        }
+
+                        if(innerWidth != lastInnerWidth){
+                            lastInnerWidth = innerWidth;
+                        } else{
+                            return;
+                        }
+
+                        // masonry Elements
+                        var masonryElements = masonryDiv.children[0].children;
+                        var masonryElementsTransclude = masonryDiv.children[0].lastElementChild.children;
+
+                        if(!masonryElements && !masonryElementsTransclude) return;
 
                         var lastHeights = [];
                         var targetLoc = 0;
@@ -44,39 +68,49 @@ export default angular
                         // build
                         var columns = 1;
                         var items = 0;
-                        if(innerWidth >= 568){
-                            columns=2;
-                            if(innerWidth >= 768){columns=3}
-                            if(innerWidth >= 1024){columns=4}
-                            if(innerWidth >= 1280){columns=5}
-                            for(var i=0; i<columns; i++){lastHeights[i] = 0;}
-                            for(var i=0; i< masornyElements.length;i++){
-                                if(masornyElements[i].localName != 'ng-transclude'){
-                                    masornyElements[i].style.width = innerWidth/columns + 'px';
-                                    masornyElements[i].style.position = 'absolute';
+
+                        var space = 0;
+                        if($ctrl.colWidth > 0){
+                            masonryDiv.children[0].style.maxWidth = innerWidth + 'px';
+                            space = $ctrl.colWidth;
+                            var fractionColumn = innerWidth / space;
+                            columns = Math.floor(fractionColumn);
+                        } else {
+                            var fractionColumn = innerWidth / 280;
+                            columns = Math.floor(fractionColumn);
+                            space = innerWidth/columns;
+                        }
+                        if(columns < 1) columns = 1;
+
+                        if(columns > 1){
+                            for(var i=0; i < columns; i++){lastHeights[i] = 0;}
+                            for(var i=0; i < masonryElements.length; i++){
+                                if(masonryElements[i].localName != 'ng-transclude'){
+                                    masonryElements[i].style.width = space + 'px';
+                                    masonryElements[i].style.position = 'absolute';
                                     items++;
                                 }
                             }
-                            for(var i=0; i< masornyElementsTransclude.length;i++){
-                                masornyElementsTransclude[i].style.width = innerWidth/columns + 'px';
-                                masornyElementsTransclude[i].style.position = 'absolute';
+                            for(var i=0; i< masonryElementsTransclude.length;i++){
+                                masonryElementsTransclude[i].style.width = space + 'px';
+                                masonryElementsTransclude[i].style.position = 'absolute';
                                 items++;
                             }
                             if($ctrl.alignment == 'center'){
                                 if(items < columns){
-                                    masornyDiv.children[0].style.maxWidth = innerWidth/columns*items + 'px';
+                                    masonryDiv.children[0].style.maxWidth = space * items + 'px';
                                 }
                             }
 
                             // Draw
-                            for(var i = 0; i < masornyElements.length; i++){
+                            for(var i = 0; i < masonryElements.length; i++){
                                 var iTop = lastHeights[targetLoc];
-                                var iLeft = targetLoc * masornyElements[i].clientWidth;
-                                masornyElements[i].style.top = iTop+'px';
-                                masornyElements[i].style.left = iLeft+'px';
+                                var iLeft = targetLoc * masonryElements[i].clientWidth;
+                                masonryElements[i].style.top = iTop+'px';
+                                masonryElements[i].style.left = iLeft+'px';
 
                                 // current height
-                                var iHeight = masornyElements[i].clientHeight;
+                                var iHeight = masonryElements[i].clientHeight;
                                 // set new lastHeight
                                 lastHeights[targetLoc] += iHeight;
 
@@ -92,14 +126,14 @@ export default angular
                             }
 
                             // Draw transclude
-                            for(var i = 0; i < masornyElementsTransclude.length; i++){
+                            for(var i = 0; i < masonryElementsTransclude.length; i++){
                                 var iTop = lastHeights[targetLoc];
-                                var iLeft = targetLoc * masornyElementsTransclude[i].clientWidth;
-                                masornyElementsTransclude[i].style.top = iTop+'px';
-                                masornyElementsTransclude[i].style.left = iLeft+'px';
+                                var iLeft = targetLoc * masonryElementsTransclude[i].clientWidth;
+                                masonryElementsTransclude[i].style.top = iTop+'px';
+                                masonryElementsTransclude[i].style.left = iLeft+'px';
 
                                 // current height
-                                var iHeight = masornyElementsTransclude[i].clientHeight;
+                                var iHeight = masonryElementsTransclude[i].clientHeight;
                                 // set new lastHeight
                                 lastHeights[targetLoc] += iHeight;
 
@@ -113,31 +147,28 @@ export default angular
                                     }
                                 }
                             }
-                            var masornyMaxHeight = lastHeights.reduce(function(a,b) {
+                            var masonryMaxHeight = lastHeights.reduce(function(a,b) {
                                 return Math.max(a,b);
                             });
-                            masornyDiv.children[0].style.minHeight = 'calc('+ masornyMaxHeight + 'px + 1em)';
+                            masonryDiv.children[0].style.minHeight = 'calc('+ masonryMaxHeight + 'px + 1em)';
 
                         } else {
-                            // not masorny
-                            for(var i=0; i<masornyElements.length;i++){
-                                masornyElements[i].style.width = 'auto';
-                                masornyElements[i].style.position = 'static';
+                            // not masonry
+                            for(var i=0; i<masonryElements.length; i++){
+                                masonryElements[i].style.width = 'auto';
+                                masonryElements[i].style.position = 'static';
                             }
-                            for(var i=0; i<masornyElementsTransclude.length;i++){
-                                masornyElementsTransclude[i].style.width = 'auto';
-                                masornyElementsTransclude[i].style.position = 'static';
+                            for(var i=0; i<masonryElementsTransclude.length; i++){
+                                masonryElementsTransclude[i].style.width = 'auto';
+                                masonryElementsTransclude[i].style.position = 'static';
                             }
-                            masornyDiv.style.minHeight = 'auto';
+                            masonryDiv.style.minHeight = 'auto';
                         }
                     }
-                    masorny();
+
+                    masonry();
                     angular.element($window).bind('resize', function(){
-                        innerWidth = masornyDiv.clientWidth;
-                        if(innerWidth != lastInnerWidth){
-                            masorny();
-                            lastInnerWidth = innerWidth;
-                        }
+                        masonry();
                     });
                 },200);
             }
